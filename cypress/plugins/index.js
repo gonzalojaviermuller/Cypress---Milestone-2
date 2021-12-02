668f7d9da466f9dc72b79c69f2aec98d1a98a529
Gonzalo Müller <gonzalojaviermuller@gmail.com>
2021-12-02 04:06:26 -0300

    practiceForm PR corrections v1

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,9 @@
 /// <reference types="cypress" />
-const { isFileExist } = require('cy-verify-downloads');
+
 var dayjs = require('dayjs')
 
 dayjs().format()
 
 module.exports = (on, config) => {
-  on('task', { isFileExist })
+  
 }
---
+++ cypress/support/selectors/practiceFormSelectors.js
@@ -0,0 +1,47 @@
+const firstNameInput = "#firstName";
+const lastNameInput = "#lastName";
+const emailInput = "#userEmail";
+const genderInput = "#gender-radio-";
+const userNumberInput = "#userNumber";
+const dateOfBirthInput = "#dateOfBirthInput";
+const monthDropdown = ".react-datepicker__month-select";
+const yearDropdown = ".react-datepicker__year-select";
+const addressInput = "#currentAddress";
+const daysContainer = '[role="listbox"]';
+const subjectsInput = "#subjectsInput";
+const selectedSubject = ".subjects-auto-complete__multi-value";
+const hobbiesInput = "#hobbies-checkbox-";
+const selectFileBtn = "#uploadPicture";
+const stateBtn = "#state";
+const cityBtn = "#city";
+const textValue = ".css-1uccc91-singleValue";
+const cityOption = "#react-select-4-option-";
+const getSubmitBtn = "#submit";
+const modalContent = ".modal-content"
+const tableRow = "tr";
+const tableColumn = "td";
+
+export default {
+  firstNameInput,
+  lastNameInput,
+  emailInput,
+  genderInput,
+  userNumberInput,
+  dateOfBirthInput,
+  monthDropdown,
+  yearDropdown,
+  addressInput,
+  daysContainer,
+  subjectsInput,
+  selectedSubject,
+  hobbiesInput,
+  selectFileBtn,
+  stateBtn,
+  cityBtn,
+  textValue,
+  cityOption,
+  getSubmitBtn,
+  modalContent,
+  tableRow,
+  tableColumn
+};
