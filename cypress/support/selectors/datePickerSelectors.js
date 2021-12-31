444e5fedda6057e83a988854ea7cb727bea40d1c
Gonzalo Müller <86006789+gonzalojaviermuller@users.noreply.github.com>
2021-12-31 17:34:36 -0300

    datePicker (#5)
    * datePicker
    
    * datePicker PR changes v1
    
    * datePicker PR changes v2
    
    * datePicker PR changes v3

---
+++ cypress/support/selectors/datePickerSelectors.js
@@ -0,0 +1,33 @@
+const datePickerInput = "#datePickerMonthYearInput";
+const dateAndTimePickerInput = "#dateAndTimePickerInput";
+const nextMonthBtn = ".react-datepicker__navigation--next";
+const prevMonthBtn = ".react-datepicker__navigation--previous";
+const monthSelector = ".react-datepicker__month-select";
+const yearSelector = ".react-datepicker__year-select";
+const currentMonthDisplay = ".react-datepicker__current-month";
+const monthDropdown = ".react-datepicker__month-read-view";
+const yearDropdown = ".react-datepicker__year-read-view";
+const monthDropdownList = ".react-datepicker__month-dropdown";
+const yearDropdownList = ".react-datepicker__year-dropdown";
+const timeList = ".react-datepicker__time-list-item";
+const selectedTime = "react-datepicker__time-list-item--selected";
+const selectedDay = "react-datepicker__day--selected";
+const daysContainer = '[role="listbox"]';
+
+export default {
+  datePickerInput,
+  dateAndTimePickerInput,
+  nextMonthBtn,
+  prevMonthBtn,
+  monthSelector,
+  yearSelector,
+  currentMonthDisplay,
+  monthDropdown,
+  yearDropdown,
+  monthDropdownList,
+  yearDropdownList,
+  timeList,
+  selectedTime,
+  selectedDay,
+  daysContainer
+};
